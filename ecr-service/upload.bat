d16e9c4d00d53850e800f5479e2d7baa57c29997
JediKnightChan <jediknightchannel@yandex.ru>
2023-05-28 21:16:26 +0300

    launcher 1.0.3

--- ecr-service/upload.bat
+++ ecr-service/upload.bat
@@ -1,2 +1,2 @@
-aws s3 cp api/ s3://ecr-service/api --recursive
-aws s3 cp index.json s3://ecr-service/index.json
+aws s3 cp api/ s3://ecr-service/api --recursive --endpoint-url=https://storage.yandexcloud.net
+aws s3 cp index.json s3://ecr-service/index.json --endpoint-url=https://storage.yandexcloud.net
