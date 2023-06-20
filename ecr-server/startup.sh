0d5e21508c59005c104f2a1916e6581cdd2fc808
JediKnightChan <jediknightchannel@yandex.ru>
2023-06-21 02:40:37 +0300

    Fix port forward, remove zip

--- ecr-server/startup.sh
+++ ecr-server/startup.sh
@@ -3,4 +3,4 @@ docker rm $(docker ps -a -q)
 docker rmi -f $(docker images -aq)
 
 docker pull cr.yandex/crp110tk8f32a48oaeqo/ecr-server:latest
-docker run --restart always -p 7777:7777 -d cr.yandex/crp110tk8f32a48oaeqo/ecr-server:latest
+docker run --restart always -p 7777:7777/udp -d cr.yandex/crp110tk8f32a48oaeqo/ecr-server:latest
