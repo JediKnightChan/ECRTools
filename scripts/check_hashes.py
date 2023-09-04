f827c931fcb51613a4dd4ff4c8b5eea9b7ff8b21
JediKnightChan <jediknightchannel@yandex.ru>
2023-09-05 00:23:50 +0300

    1.2.10

--- scripts/check_hashes.py
+++ scripts/check_hashes.py
@@ -14,13 +14,19 @@ def md5(filename):
     return hash_md5.hexdigest()
 
 
-tag = "prod_1.2.9"
+DO_OVERWRITE_GAME_DATA = True
+tag = "prod_1.2.10"
 root_root_dir = f"C:/Users/JediKnight/Documents/Unreal Projects/ECRPackagedShipping/{tag}/"
 game_dir = os.path.join(root_root_dir, "Windows")
 archive = os.path.join(root_root_dir, "game.zip")
 
+archive_hash = ""
+archive_size = ""
+
 if os.path.exists(archive):
-    print("Archive", md5(archive), "size", os.stat(archive).st_size)
+    archive_hash = md5(archive)
+    archive_size = os.stat(archive).st_size
+    print("Archive", archive_hash, "size", archive_size)
 
 files = [
     'ECR.exe'
@@ -53,3 +59,14 @@ for folder in folders:
         result[os.path.join(folder, file)] = hash
 
 print(json.dumps(result, indent=5))
+
+if DO_OVERWRITE_GAME_DATA:
+    with open("../ecr-service/api/ecr/game_data.json", "r") as f:
+        data = json.load(f)
+
+    data["branch_prod"]["complete_archives"]["Windows"]["verify_files"] = result
+    data["branch_prod"]["complete_archives"]["Windows"]["hash"] = archive_hash
+    data["branch_prod"]["complete_archives"]["Windows"]["size"] = archive_size
+
+    with open("../ecr-service/api/ecr/game_data.json", "w") as f:
+        json.dump(data, f, indent=2)
