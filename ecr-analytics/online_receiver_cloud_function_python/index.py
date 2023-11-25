2bf4410685f1ed1aa93e032efdbfc5d23e28119b
JediKnightChan <jediknightchannel@yandex.ru>
2023-11-25 17:03:52 +0300

    Don't exclude matches by their creation time, instead by match started time

--- ecr-analytics/online_receiver_cloud_function_python/index.py
+++ ecr-analytics/online_receiver_cloud_function_python/index.py
@@ -75,7 +75,7 @@ def update_current_online_stats(raw_online_data, destroy=False):
     for k, v in latest_matches_data.items():
         if time.time() - v.get("latest_match_update_ts", 0) > 60 * 5:
             continue
-        if time.time() - float(v.get("match_creation_ts", 0)) > 60 * 30:
+        if time.time() - float(v.get("started_ts", 0)) > 60 * 30:
             continue
         new_latest_matches_data[k] = v
 
