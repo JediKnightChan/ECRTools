46ee39266f67b88eb0e14b5a4fadf0af9568013f
JediKnightChan <jediknightchannel@yandex.ru>
2024-02-27 00:44:09 +0300

    1.7.3

--- ecr-analytics/example/s3_downloader.py
+++ ecr-analytics/example/s3_downloader.py
@@ -110,7 +110,7 @@ def __prev_str(s):
 
 contour = "prod"
 do_remove = True
-patch = "1.7.0.4"
+patch = "1.7.2"
 s3_keys = s3list(f"ecr-game/{contour}/{patch}/raw", recursive=False)
 
 if do_remove:
